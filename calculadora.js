bfe0b11f6c56a7adc43d33ead0b9815b65f0d1d3
GERALDINE <geraldineventurariveros@gmail.com>
2022-12-13 21:30:49 -0300

    se agrego calcu.js

---
+++ calculadora.js
@@ -0,0 +1,50 @@
+console.log("java scrip conect exitosamente")
+const txtop1 =document.getElementById("op1")
+const txtOperacion=document.getElementById("operacion")
+const txtop2 =document.getElementById("op2")
+const btnCalcular= document.getElementById("calcular")
+const pResultado= document.getElementById("resultado")
+
+btnCalcular.addEventListener("click", calcular)
+
+function calcular(){
+//console.log(txtOperacion.Value)
+
+  const operacion= txtOperacion.value 
+  const op1 = parseFloat (txtop1.value)
+  const op2 = parseFloat (txtop2.value)
+  
+   if(operacion =="+"||operacion=="-"||operacion=="+"||operacion=="/"&&!isNaN(op1)&&!isNaN(op2)){
+    let resultado;
+    switch (operacion){
+        case"+":
+            resultado = op1 + op2
+            break;
+         case"-":
+            resultado = op1 - op2
+            break;
+        case"*":
+            resultado = op1 *op2
+            break;
+        case"/":
+            resultado = op1 / op2
+            break;
+       }
+    pResultado.style= "color:black"  
+    pResultado.innerText= "= " + resultado
+  }else{
+    pResultado.style= "color:red"  
+    pResultado.innerText= "calculo imposible"
+ }
+}
+   
+
+    
+ 
+ 
+ 
+ 
+
+
+
+
